1a5b188992ab2f7265bf899ff11473849be26ac1
Chase Hickman <ChaseJHickman69@gmail.com>
2020-12-14 17:00:00 -0600

    Added Character.cpp & .h same with Game
    Essentially made a bunch of initialized variables

--- IntroToCPP/Main.cpp
+++ IntroToCPP/Main.cpp
@@ -2,59 +2,27 @@
 
 int main()
 {
-	char answer;
-	int num;
-	int min;
-	int max;
+	char player1Name[20];
+	char player1Token = 'X';
+	bool player1Win = false;
 
-	std::cout << "Pick a number between one and one hundred" << std::endl;
+	char player2Name[20];
+	char player2Token = 'O';
+	bool player2Win = false;
+
+	std::cout << "Player 1, what is your name?" << std::endl;
+	std::cin >> player1Name;
+	system("CLS");
+
+	std::cout << "Player 2, what is your name?" << std::endl;
+	std::cin >> player2Name;
+	system("CLS");
+
+	std::cout << player1Name << std::endl;
+	std::cout << player2Name << std::endl;
 
 	system("pause");
-
-	std::cout << "Rules: Answer all questions with either Y for yes or N for no!" << std::endl;
-
-	std::cout << " You must also answer G for greater than or L for less than!" << std::endl;
-
-	std::cout << "Remeber that number and DO NOT CHANGE IT!" << std::endl;
-
-	system("pause");
-	system("cls");
-
-	std::cout << "Now let's get started! Is your number 50?" << std::endl;
-
-	int num = 50;
-	int min = 0;
-	int max = 100;
-
-	void mid(int min, int max);
-	{
-		(min + max) / 2;
-	}
-
-	std::cin >> answer;
-
-	if (answer != 'Y' && answer != 'N' && answer != 'G' && answer != 'L')
-	{
-		if (answer == 'Y')
-		{
-			std::cout << "Your number is " << num << "!" << std::endl;
-
-			system("pause");
-
-			return 1;
-		}
-		else if (answer == 'N')
-		{
-			std::cout << "Is the number greater or less than 50?" << std::endl;
-
-			system("pause");
-
-			if (answer == 'G')
-			{
-				mid(51, 100);
-			}
-		}
-	}
+	system("CLS");
 
 	system("pause");
 	return 1;
